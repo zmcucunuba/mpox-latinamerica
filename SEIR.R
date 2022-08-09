ccf94eb54965598cd59afb6004360680fa064778
Zulma Cucunubá <zulmacucunuba@Zulmas-MacBook-Pro.local>
2022-08-08 19:16:38 -0500

    first SEIR model

---
+++ SEIR.R
@@ -0,0 +1,16 @@
+
+
+
+p     <- 0.8   # Probabilidad p de ser infeccioso cuando el encuentro se hace con alguien que está contagiado
+b_gg  <- 1     # Num contactos general-general
+b_gh  <- 0.05  # Num contactos general-general
+b_gp  <- 1e-6  # Num contactos general-pr
+b_hg  <- 0.05  # Num contactos HSH-general
+b_hh  <- 0.25  # Num contactos general-pr
+b_hp  <- 1e-2  # Num contactos general-pr
+b_pg  <- 1e-6  # Num contactos general-pr
+b_ph  <- 1e-2  # Num contactos general-pr
+b_pp  <- 1     # Num contactos general-pr
+N_g   <- 1.e6  # Num contactos general-pr
+N_h   <- 2.e4  # Población humana total
+N_p   <- 1.5e2 # Num contactos general-pr
