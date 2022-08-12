851efe0b42e7bf202950756f8b129dedadf3ec5c
Zulma Cucunubá <zulmacucunuba@Zulmas-MacBook-Pro.local>
2022-08-12 14:50:08 -0500

    data UK

---
+++ UK_data.R
@@ -0,0 +1,11 @@
+
+# Script para entender los datos de UK
+library(tidyverse)
+library(haven)
+
+
+# nat2000 <- read_sav("data/natsal_2000_for_archive.sav")
+nat2010 <- read_sav("data/eul_level_natsal_2010_web_data_for_archive.sav")
+wave1 <- read_sav("data/natsal_covid_wave_1_archive.sav")
+nat2010arch <- read_sav("data/eul_natsal_2010_for_archive.sav")
+
