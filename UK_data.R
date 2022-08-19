a45279a90658dc6107b429d245ad9194dc39f3fc
Zulma Cucunubá <zulmacucunuba@Zulmas-MacBook-Pro.local>
2022-08-19 00:15:35 -0500

    epi data

--- UK_data.R
+++ UK_data.R
@@ -6,10 +6,10 @@ library(tidyverse)
 library(haven)
 
 
-nat2000 <- read_sav("data/natsal_2000_for_archive.sav")
-nat2010 <- read_sav("data/eul_level_natsal_2010_web_data_for_archive.sav")
-wave1 <- read_sav("data/natsal_covid_wave_1_archive.sav")
-nat2010arch <- read_sav("data/eul_natsal_2010_for_archive.sav")
+nat2000 <- read_sav("data/contacts/natsal_2000_for_archive.sav")
+nat2010 <- read_sav("data/contacts/eul_level_natsal_2010_web_data_for_archive.sav")
+wave1 <- read_sav("data/contacts/natsal_covid_wave_1_archive.sav")
+nat2010arch <- read_sav("data/contacts/eul_natsal_2010_for_archive.sav")
 
 str(wave1)
 
